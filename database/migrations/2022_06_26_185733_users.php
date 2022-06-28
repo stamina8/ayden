99383fd56ab700d00788320dd997ecff144f05f5
NyStamina <707259967@qq.com>
2022-06-28 10:35:46 +0800

    Update 2022_06_26_185733_users.php
    users

--- database/migrations/2022_06_26_185733_users.php
+++ database/migrations/2022_06_26_185733_users.php
@@ -13,7 +13,7 @@ class Users extends Migration
      */
     public function up()
     {
-        Schema::create('rental', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
