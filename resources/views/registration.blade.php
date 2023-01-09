20f101c2cbb52b82f2cf560b5d9df9616295d1fc
Ayden002 <zhu0026@hz.nl>
2023-01-09 13:57:25 +0100

    new2

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,82 +1,176 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+
+@stamina8
+stamina8
+/
+ayden
+Public
+forked from Ayden002/Portfolio-Ayden
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+ayden/resources/views/registration.blade.php
+@Ayden002
+Ayden002 new1
+Latest commit 8a9d397 20 minutes ago
+History
+2 contributors
+@Ayden002@stamina8
+82 lines (76 sloc)  4.27 KB
+
 @extends('layouts.nav')
 @section('content')
 
-        <link rel="stylesheet" href="{{asset('css/registration.css')}}">
-        <title>Services</title>
+    <link rel="stylesheet" href="{{asset('css/registration.css')}}">
+    <title>Services</title>
 
-        <!--background-->
-        <div class="background-container2">
-            <img class="com" src="img/p3.jpeg">
-            <div class="shadow-white"></div>
-            <h1><span class=heading style="color:dimgray" >Services</span></h1><br>
-            <!--form-->
-               <form method="POST" action="{{ route('register') }}" class="form-row">
-               @method('post')
-               @csrf
-                <label for="fname">Name111:</label>
-                <input type="text" id="name" name="name" placeholder="Type your Name"><br>
-                   @if ($errors->has('name'))
-                       <span class="text-danger">{{ $errors->first('name') }}</span>
-                   @endif
-                   <!--
-                <label for="lname">Last name:</label>
-                <input type="text" id="lname" name="lname" placeholder="Type your Last Name"><br>
-     -->
-                <label for="Email">Email:</label>
-                <input type="text" id="Email" name="email" placeholder="Type Email address"><br>
-                   @if ($errors->has('email'))
-                       <span class="text-danger">{{ $errors->first('email') }}</span>
-                   @endif
-                <label for="Phone number">Phone number:</label>
-                <input type="text" id="pnumber" name="phone_number" placeholder="Type your Phone number"><br>
-                   @if ($errors->has('phone_number'))
-                       <span class="text-danger">{{ $errors->first('phone_number') }}</span>
-                   @endif
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" placeholder="Set password">
-                   @if ($errors->has('password'))
-                       <span class="text-danger">{{ $errors->first('password') }}</span>
-                   @endif
-                   <br>
-   <!--              <input type="password" id="password" name="password"
-                pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!#])[A-Za-z\d@$!#]{5,10}$"
-                title="Notice:Password should be 5-10 characters in length contains at least 1 lowercase letter, 1 uppercase letter, 1 number and one of the following special characters such as !,@,#,$."
-                placeholder="Set password"><br> -->
+    <!--background-->
+    <div class="background-container2">
+        <img class="com" src="img/p3.jpeg">
+        <div class="shadow-white"></div>
+        <h1><span class=heading style="color:dimgray" >Services</span></h1><br>
+        <!--form-->
+        <form method="POST" action="{{ route('register') }}" class="form-row">
+            @method('post')
+            @csrf
+{{--            old--}}
+{{--            <label for="fname">Name:</label>--}}
+{{--            <input type="text" id="name" name="name" placeholder="Type your Name">--}}
+{{--            @if ($errors->has('name'))--}}
+{{--                <span class="text-danger">{{ $errors->first('name') }}</span>--}}
+{{--            @endif--}}
 
-                <label for="confirm password">Confirm Password:</label>
-                <input type="password" id="confirm_password" name="confirm_password" placeholder="Set password">
-                   @if ($errors->has('confirm_password'))
-                       <span class="text-danger">{{ $errors->first('confirm_password') }}</span>
-                   @endif
-                   <br>
-                <label for="fname">Are you a student?</label>
-                <select name="is_student" >
-                  <option value="0">No</option>
-                  <option value="1">Yes</option>
-                </select>
-                <br>
-                <label for="confirm password">Identity:</label>
-                <select name="type" >
-                  <option value="customers">customers</option>
-                  <option value="staff">staff</option>
-                  <option value="web_manager">web_manager</option>
-                </select>
-                <br>
-                <span id='message'></span><br>
+{{--            <label for="Email">Email:</label>--}}
+{{--            <input type="text" id="Email" name="email" placeholder="Type Email address">--}}
+{{--            @if ($errors->has('email'))--}}
+{{--                <span class="text-danger">{{ $errors->first('email') }}</span>--}}
+{{--            @endif--}}
+{{--            <label for="Phone number">Phone number:</label>--}}
+{{--            <input type="text" id="pnumber" name="phone_number" placeholder="Type your Phone number">--}}
+{{--            @if ($errors->has('phone_number'))--}}
+{{--                <span class="text-danger">{{ $errors->first('phone_number') }}</span>--}}
+{{--            @endif--}}
+{{--            <label for="password">Password:</label>--}}
+{{--            <input type="password" id="password" name="password" placeholder="Set password">--}}
+{{--            @if ($errors->has('password'))--}}
+{{--                <span class="text-danger">{{ $errors->first('password') }}</span>--}}
+{{--            @endif--}}
+{{--            --}}
+{{--            <label for="confirm password">Confirm Password:</label>--}}
+{{--            <input type="password" id="confirm_password" name="confirm_password" placeholder="Set password">--}}
+{{--            @if ($errors->has('confirm_password'))--}}
+{{--                <span class="text-danger">{{ $errors->first('confirm_password') }}</span>--}}
+{{--            @endif--}}
+{{--            old end--}}
 
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-               <!--  <label for="terms" style="font-size: 15px;">Agreed the terms and conditions: </label> -->
-               <!--  <input type="checkbox" id="terms" name="terms"><br><br> -->
 
-                <input type="submit" value="Register">
-                <a href="{{url('/')}}" style="color: inherit; text-decoration: none;">Cancel</a>
-                    <span id='message'></span><br>
-            </form>
-        </div>
+{{--            new--}}
+            <div class="form-group row">
+                <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+                <div class="col-md-6">
+                    <input type="text" id="name" class="form-control" name="name" required autofocus>
+                    @if ($errors->has('name'))
+                        <span class="text-danger">{{ $errors->first('name') }}</span>
+                    @endif
+                </div>
+            </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"intgrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"crossorigin="anonymous"></script>
-        <!--javascript-->
-{{--        <script type="text/javascript" src="{{asset('js/registration.js')}}"></script>--}}
+            <div class="form-group row">
+                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                <div class="col-md-6">
+                    <input type="text" id="email_address" class="form-control" name="email" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="email_address" class="col-md-4 col-form-label text-md-right">phone_number</label>
+                <div class="col-md-6">
+                    <input type="text" id="email_address" class="form-control" name="phone_number" required autofocus>
+                    @if ($errors->has('phone_number'))
+                        <span class="text-danger">{{ $errors->first('phone_number') }}</span>
+                    @endif
+                </div>
+            </div>
+
+
+
+            <div class="form-group row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                <div class="col-md-6">
+                    <input type="password" id="password" class="form-control" name="password" required>
+                    @if ($errors->has('password'))
+                        <span class="text-danger">{{ $errors->first('password') }}</span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">confirm_password</label>
+                <div class="col-md-6">
+                    <input type="password" id="password" class="form-control" name="confirm_password" required>
+                    @if ($errors->has('confirm_password'))
+                        <span class="text-danger">{{ $errors->first('confirm_password') }}</span>
+                    @endif
+                </div>
+            </div>
+{{--            new end--}}
+            <label for="fname">Are you a student?</label>
+            <select name="is_student" >
+                <option value="0">No</option>
+                <option value="1">Yes</option>
+            </select>
+
+            <label for="confirm password">Identity:</label>
+            <select name="type" >
+                <option value="customers">customers</option>
+                <option value="staff">staff</option>
+                <option value="web_manager">web_manager</option>
+            </select>
+            <br>
+            <span id='message'></span><br>
+
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <!--  <label for="terms" style="font-size: 15px;">Agreed the terms and conditions: </label> -->
+            <!--  <input type="checkbox" id="terms" name="terms"><br><br> -->
+
+            <input type="submit" value="Register">
+            <a href="{{url('/')}}" style="color: inherit; text-decoration: none;">Cancel</a>
+            <span id='message'></span><br>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"intgrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"crossorigin="anonymous"></script>
+    <!--javascript-->
+    {{--        <script type="text/javascript" src="{{asset('js/registration.js')}}"></script>--}}
     </body>
-</html>
+    </html>
 @endsection
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+ayden/registration.blade.php at master · stamina8/ayden
