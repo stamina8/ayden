8a9d39734cdcfcac3247a7a5f6f361badccaa8be
Ayden002 <zhu0026@hz.nl>
2023-01-09 13:31:22 +0100

    new1

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -76,7 +76,7 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"intgrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"crossorigin="anonymous"></script>
         <!--javascript-->
-        <script type="text/javascript" src="{{asset('js/registration.js')}}"></script>
+{{--        <script type="text/javascript" src="{{asset('js/registration.js')}}"></script>--}}
     </body>
 </html>
 @endsection
